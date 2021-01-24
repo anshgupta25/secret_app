26ed5c7b21dc673a2b04929f7d363042ea1b15b1
anshgupta25 <72184458+anshgupta25@users.noreply.github.com>
2021-01-24 16:19:50 +0530

    pushing to heroku

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ansh:ansh123@cluster0.tirrg.mongodb.net/secDB?retryWrites=true&w=majority", {useNewUrlParser: true});
 mongoose.set("useCreateIndex", true);
 
 const userSchema = new mongoose.Schema ({
